e9de6ecdee689553169919269980dde53850727f
Yosua Vito Agung Pratama <220711893@students.uajy.ac.id>
2024-09-11 15:28:09 +0700

    Update Tugas_Modul 1_B_YOSUA_11893.py

--- Tugas_Modul 1_B_YOSUA_11893.py
+++ Tugas_Modul 1_B_YOSUA_11893.py
@@ -4,10 +4,12 @@ import pickle
 import os
 
 #load model
-model_directory = r'C:\Users\iamyo\OneDrive\Documents\TUGAS\MachineLearning' ##diisi dengan path folder dimana file model berada
+#model_directory = r'C:\Users\iamyo\OneDrive\Documents\TUGAS\MachineLearning' ##diisi dengan path folder dimana file model berada
 
 # Gunakan os.path.join() untuk menggabungkan direktori dan file model pickle
-model_path = os.path.join(model_directory, 'rf_diabetes_model.pkl')
+#model_path = os.path.join(model_directory, 'rf_diabetes_model.pkl')
+
+model_path = 'rf_diabetes_model.pkl'
 
 # Periksa apakah file ada di direktori yang ditentukan
 if os.path.exists(model_path):
